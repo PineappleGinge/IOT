9dbd8a80201267c83ac738f123305e63290ee103
PiotrScripts <98274138+PiotrScripts@users.noreply.github.com>
2024-03-13 20:33:56 +0000

    Actual initial commit

---
+++ AI.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TigerBot
+{
+    public class AI : IDisposable
+    {
+        private const int IMAGE_SIZE = 640;
+        private const int NUM_DETECTIONS = 8400;
+        
+        public double minConfidence = 0.5;
+        private Bitmap screenCaptureBitmap = null;
+        private readonly RunOptions modeloptions;
+        private InferenceSession onnxModel;
+        private List<string> outputNames;
+
+        private const int OFFSET = IMAGE_SIZE / 2;
+        private Rectangle detectionBox = new Rectangle((Screen.PrimaryScreen.Bounds.Width / 2) - OFFSET / 2, (Screen.PrimaryScreen.Bounds.Height / 2) - OFFSET / 2, IMAGE_SIZE, IMAGE_SIZE);
+
+        public AI(string modelPath)
+        {
+            modeloptions = new RunOptions();
+
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                EnableCpuMemArena = true,
+                EnableMemoryPattern = true,
+                GraphOptimizationLevel = GraphOptimizationLevel.ORT_ENABLE_ALL,
+                ExecutionMode = ExecutionMode.ORT_PARALLEL
+            };
+
+            try
+            {
+                sessionOptions.AppendExecutionProvider_DML();
+                onnxModel = new InferenceSession(modelPath, sessionOptions);
+                outputNames = onnxModel.OutputMetadata.Keys.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load onnx model.\n{ex}");
+                Environment.Exit(1);
+            }
+        }
+
+        public int[] GetEnemy()
+        {
+            Bitmap frame = ScreenGrab();
+            float[] inputArray = BitmapToFloatArray(frame);
+            if (inputArray == null) { return null; }
+
+            Tensor<float> inputTensor = new DenseTensor<float>(inputArray, new int[] { 1, 3, frame.Height, frame.Width });
+            List<NamedOnnxValue> inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("images", inputTensor) };
+            var results = onnxModel.Run(inputs, outputNames, modeloptions);
+
+            Tensor<float> outputTensor = results[0].AsTensor<float>();
+            List<int> filteredIndices = Enumerable.Range(0, NUM_DETECTIONS).AsParallel().Where(i => outputTensor[0, 4, i] >= minConfidence).ToList();
+
+            int[] closest = new int[3] { int.MaxValue, int.MaxValue, int.MaxValue };
+
+            foreach (int i in filteredIndices)
+            {
+                float confidence = outputTensor[0, 4, i];
+
+                float centerX = outputTensor[0, 0, i];
+                float centerY = outputTensor[0, 1, i];
+                float width = outputTensor[0, 2, i];
+                float height = outputTensor[0, 3, i];
+
+                int mouseX = (int)Math.Round(OFFSET / 2 - centerX);
+                int mouseY = (int)Math.Round(OFFSET / 2 - centerY);
+
+                int distance = (int)Math.Round(Math.Sqrt(Math.Pow(mouseX, 2) + Math.Pow(mouseY, 2)));
+
+                if (distance < closest[2])
+                    closest = new int[3] { -mouseX, -mouseY, distance };
+            }
+
+            if (closest[2] == int.MaxValue)
+                return null;
+
+            return closest;
+        }
+
+        public Bitmap ScreenGrab()
+        {
+            if (screenCaptureBitmap == null || screenCaptureBitmap.Width != detectionBox.Width || screenCaptureBitmap.Height != detectionBox.Height)
+            {
+                screenCaptureBitmap?.Dispose();
+                screenCaptureBitmap = new Bitmap(detectionBox.Width, detectionBox.Height);
+            }
+
+            using (var g = Graphics.FromImage(screenCaptureBitmap))
+            {
+                g.CopyFromScreen(detectionBox.Left, detectionBox.Top, 0, 0, detectionBox.Size);
+            }
+            return screenCaptureBitmap;
+        }
+
+        public static float[] BitmapToFloatArray(Bitmap image)
+        {
+            int height = image.Height;
+            int width = image.Width;
+            float[] result = new float[3 * height * width];
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = image.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            IntPtr ptr = bmpData.Scan0;
+            int bytes = Math.Abs(bmpData.Stride) * height;
+            byte[] rgbValues = new byte[bytes];
+
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+            for (int i = 0; i < rgbValues.Length / 3; i++)
+            {
+                int index = i * 3;
+                result[i] = rgbValues[index + 2] / 255.0f;
+                result[height * width + i] = rgbValues[index + 1] / 255.0f;
+                result[2 * height * width + i] = rgbValues[index] / 255.0f;
+            }
+
+            image.UnlockBits(bmpData);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            onnxModel?.Dispose();
+            screenCaptureBitmap?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ V4.ino
@@ -0,0 +1,175 @@
+#include <Mouse.h> 
+#include <Wire.h> 
+#include <SPI.h> 
+#include <usbhub.h> 
+#include <hidboot.h> 
+
+USB Usb; 
+USBHub Hub(&Usb); 
+HIDBoot<USB_HID_PROTOCOL_MOUSE> HidMouse(&Usb);
+
+String myString; 
+
+int arr[2]; 
+int arrv[8]; 
+
+int j = 0;
+int c = 0;
+int e = 0;
+
+int lmb = 0;
+int rmb = 0;
+int mmb = 0;
+
+int dx; 
+int dy;
+
+class MouseRptParser : public MouseReportParser 
+{ 
+    protected: 
+        void OnMouseMove  (MOUSEINFO *mi); 
+        void OnLeftButtonUp (MOUSEINFO *mi); 
+        void OnLeftButtonDown (MOUSEINFO *mi); 
+        void OnRightButtonUp  (MOUSEINFO *mi); 
+        void OnRightButtonDown  (MOUSEINFO *mi); 
+        void OnMiddleButtonUp (MOUSEINFO *mi); 
+        void OnMiddleButtonDown (MOUSEINFO *mi); 
+};
+void MouseRptParser::OnMouseMove(MOUSEINFO *mi) 
+{ 
+    dx = mi->dX; 
+    dy = mi->dY; 
+}; 
+void MouseRptParser::OnLeftButtonUp (MOUSEINFO *mi) 
+{ 
+    lmb = 0; 
+}; 
+void MouseRptParser::OnLeftButtonDown (MOUSEINFO *mi) 
+{ 
+    lmb = 1; 
+}; 
+void MouseRptParser::OnRightButtonUp  (MOUSEINFO *mi) 
+{ 
+    rmb = 0; 
+}; 
+void MouseRptParser::OnRightButtonDown  (MOUSEINFO *mi) 
+{ 
+    rmb = 1; 
+}; 
+void MouseRptParser::OnMiddleButtonUp (MOUSEINFO *mi) 
+{ 
+    mmb = 0; 
+}; 
+void MouseRptParser::OnMiddleButtonDown (MOUSEINFO *mi) 
+{ 
+    mmb = 1; 
+}; 
+MouseRptParser Prs;
+
+void setup() { 
+    delay(5000); 
+    Mouse.begin(); 
+    Serial.begin(115200); 
+    Serial.setTimeout(1);   
+    Usb.Init(); 
+    HidMouse.SetReportParser(0, &Prs); 
+}
+
+void loop() { 
+    dx = 0; 
+    dy = 0; 
+    j = 0; 
+    c = 0; 
+    e = 0; 
+    Usb.Task(); 
+
+    if (lmb == 0){ 
+        Mouse.release(MOUSE_LEFT); 
+    } 
+    else if (lmb == 1){ 
+        Mouse.press(MOUSE_LEFT); 
+    } 
+    if (rmb == 0){ 
+        Mouse.release(MOUSE_RIGHT); 
+    } 
+    else if (rmb == 1){ 
+        Mouse.press(MOUSE_RIGHT); 
+    } 
+    if (mmb == 0){ 
+        Mouse.release(MOUSE_MIDDLE); 
+    } 
+    else if (mmb == 1){ 
+        Mouse.press(MOUSE_MIDDLE); 
+    } 
+
+    if (Serial.available()){ 
+        myString = Serial.readString();
+
+        if (myString.substring(0, 1) == "c"){
+            Mouse.click();
+        } else if (myString.substring(0, 1) == "m"){
+            myString = myString.substring(1, myString.length() - 1);
+
+            for (int k = 0; k <= 1; k++){     
+                String val = getValue(myString, ':', k);       
+                arr[k] = val.toInt(); 
+            } 
+
+            arr[0] += dx; 
+            arr[1] += dy; 
+            for (e = 0; e < (sizeof(arr) / sizeof(arr[0])); e++) {    
+                if (arr[e] > 0){ 
+                    for (j = 0; j <= (arr[e] / 127); j++){          
+                        c = j * 127;          
+                        if (int(arr[e]) > (c + 127)){             
+                            arrv[j] = 127;                                                                
+                        } 
+                        else { 
+                            arrv[j] = arr[e] - c;                              
+                        }              
+                    }                                        
+                } 
+                else { 
+                    for (j = 0; j <= (arr[e] / -127); j++){          
+                        c = j * -127;          
+                        if (arr[e] < (c - 127)){ 
+                            arrv[j] = -127;                                            
+                        } 
+                        else { 
+                            arrv[j] = arr[e] - c;                      
+                        }                             
+                    }         
+                }  
+                for (int p = 0; p < j; p++){ 
+                    if (e == 0){ 
+                        //Serial.println("X: " + String(m)); 
+                        Mouse.move(arrv[p], 0);
+                    } 
+                    else if (e == 1){ 
+                        //Serial.println("Y: " + String(m)); 
+                        Mouse.move(0, arrv[p]); 
+                    }                        
+                } 
+                memset(arrv, 0, sizeof(arrv));   
+            }
+        }
+    }
+    else { 
+        Mouse.move(dx, dy); 
+    } 
+}
+
+String getValue(String data, char separator, int index){ 
+    int found = 0; 
+    int strIndex[] = { 0, -1 }; 
+    int maxIndex = data.length() - 1; 
+ 
+    for (int i = 0; i <= maxIndex && found <= index; i++) { 
+        if (data.charAt(i) == separator || i == maxIndex) { 
+            found++; 
+            strIndex[0] = strIndex[1] + 1; 
+            strIndex[1] = (i == maxIndex) ? i+1 : i; 
+        } 
+    } 
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : ""; 
+}
